ebd2f9deda651ffa02157f5e7a7650656cbf8fab
Anusan <kailaianu21@gmail.com>
2020-08-18 22:53:19 +0530

    completed html files

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+var navigation = '<a href="index.html" class="logo">HOME</a>\
+    <input class="menu-btn" type="checkbox" id="menu-btn" />\
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>\
+    <ul class="menu">\
+      <li><a href="attraction.html">Attractions</a></li>\
+      <li><a href="accommodation.html">Accommodation</a></li>\
+      <li><a href="transport.html">Transport</a></li>\
+      <li><a href="contact.html">Contact us</a></li>\
+    </ul>';
+
+document.getElementById('nav').innerHTML = navigation;
