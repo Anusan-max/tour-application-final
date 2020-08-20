b42b49880c1a65389b770e4fa25e960f482c0b26
Anusan <kailaianu21@gmail.com>
2020-08-20 14:29:48 +0530

    finished design 1 and 2

--- js/nav.js
+++ js/nav.js
@@ -5,7 +5,7 @@ var navigation = '<a href="index.html" class="logo">HOME</a>\
       <li><a href="attraction.html">Attractions</a></li>\
       <li><a href="accommodation.html">Accommodation</a></li>\
       <li><a href="transport.html">Transport</a></li>\
-      <li><a href="contact.html">Contact us</a></li>\
+      <li><a href="contactus.html">Contact us</a></li>\
     </ul>';
 
 document.getElementById('nav').innerHTML = navigation;
